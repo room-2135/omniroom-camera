8789a05a6d1b77ffc148100884745ac78ceb07a6
gnikwo <gnikwo@hotmail.com>
2020-02-14 10:54:59 +0100

    Fix typo

--- rpicam.sh
+++ rpicam.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-./omniroom-camera --local-id `ip a s $2 | grep ether | awk '{$1=$1};1' | sed 's/://g' | cut -d ' ' -f 2` --input-stream 'rpicamsrc bitrate=1000000 ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse' $@
+./omniroom-camera --local-id `ip a s $1 | grep ether | awk '{$1=$1};1' | sed 's/://g' | cut -d ' ' -f 2` --input-stream 'rpicamsrc bitrate=1000000 ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse' $@
